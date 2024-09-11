3d6f1edeb06b1569745abef57bb1b23fe6b45203
pankaj ram pawar <pankajpawars123@gmail.com>
2024-09-11 15:43:21 +0530

    initializing

---
+++ lapms/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center text-6xl font-bold">
+      Laser Assisted Physiotherapy Monitoriing System
+    </div>
+  )
+}
