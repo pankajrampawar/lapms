5d158bf731503a56271fa6521d66183fff394da4
pankaj ram pawar <pankajpawars123@gmail.com>
2024-09-12 16:29:19 +0530

    minor changes

--- lapms/app/page.js
+++ lapms/app/page.js
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center text-6xl font-bold">
-      Laser Assisted Physiotherapy Monitoriing System
+    <div className="w-full h-screen flex flex-col gap-10 items-center justify-center text-6xl font-bold">
+      <div>
+        Laser Assisted Physiotherapy Monitoring System
+      </div>
+      <div className="text-4xl">
+        balaks joined
+      </div>
     </div>
   )
 }
